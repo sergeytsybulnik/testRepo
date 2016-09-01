c53474cea94592926ef8557b5c39d2ffc9a7c208
Sergey Tsybulnik <sergeytsybulnik@Sergeys-MacBook-Pro.local>
2016-09-01 10:29:19 +0300

    yo man

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,12 @@
+var gulp   = require('gulp');
+var config = require('../config');
+
+gulp.task('watch', 
+    ['copy:watch',
+    
+    'jade:watch',
+    'sprite:svg:watch',
+    'svgo:watch',
+    'webpack:watch',
+    'sass:watch'
+]);
